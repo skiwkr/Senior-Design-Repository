378bfc1d1d8a11d5de379d7ccd53285172a61200
Block <blockjm@ad.msoe.edu>
2019-10-28 11:51:36 -0500

    Zach smells like garlic

--- IR_Sensor_Testing_V2/Debug/subdir_rules.mk
+++ IR_Sensor_Testing_V2/Debug/subdir_rules.mk
@@ -5,10 +5,10 @@
 SHELL = cmd.exe
 
 # Each subdirectory must supply rules for building sources it contributes
-%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: ARM Compiler'
-	"C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/ti/ccs920/ccs/ccs_base/arm/include" --include_path="C:/Users/Zach/Documents/CodeComposerWorkspace/Senior-Design-Repository/MSOE_LIB" --include_path="C:/ti/ccs920/ccs/ccs_base/arm/include/CMSIS" --include_path="C:/Users/Zach/Documents/CodeComposerWorkspace/Senior-Design-Repository/IR_Sensor_Testing_V2" --include_path="C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/ti/ccs910/ccs/ccs_base/arm/include" --include_path="C:/Users/blockjm/Documents/Workspace/Senior-Design-Repository/MSOE_LIB" --include_path="C:/ti/ccs910/ccs/ccs_base/arm/include/CMSIS" --include_path="C:/Users/blockjm/Documents/Workspace/Senior-Design-Repository/IR_Sensor_Testing_V2" --include_path="C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include" --advice:power=all --define=__MSP432P401R__ --define=ccs -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
